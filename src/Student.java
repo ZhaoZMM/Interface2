55c923763d8479bdfbb7e35ffbba0ccdd6274279
ZhaoZMM <146093207+ZhaoZMM@users.noreply.github.com>
2024-05-15 12:22:04 -0400

    Update Student.java

--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class Student {
+public class Student implements doSomething{
+   private String name;
+   private double gpa;
+
+
+   public Student(String name, double gpa) {
+       this.name = name;
+       this.gpa = gpa;
+   }
+
+
+   @Override
+   public String toString() {
+       return name + " " + gpa;
+   }
+
+
+   public int weirdNum(){
+       return (int)(gpa+gpa%gpa*gpa/gpa);
+   }
+   public void saySomething(){
+       System.out.println("where am i again?");
+   }
 }
