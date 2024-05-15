59b8082e0d2bf0735acfe403585ba52bdd1f0984
ZhaoZMM <146093207+ZhaoZMM@users.noreply.github.com>
2024-05-15 12:21:53 -0400

    Update Vehicle.java

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class Vehicle {
+  private String name;
+   private int wheels;
+
+
+   public Vehicle(String name, int wheels) {
+       this.name = name;
+       this.wheels = wheels;
+   }
+
+
+   public String getName() {
+       return name;
+   }
+
+
+   public int getWheels() {
+       return wheels;
+   }
+
+
+   public void turn() {
+       System.out.println("I just turned");
+   }
+
+
+   public void brake() {
+       System.out.println("I just braked");
+   }
+
+
+   public int weirdNum(){
+       return wheels*wheels-wheels%wheels*wheels;
+   }
+   public void saySomething(){
+       System.out.println("oops, almost crashed...");
+   }
 }
