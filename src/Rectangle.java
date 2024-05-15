ef310183207630a742a97daa81c28aebcb8d5c77
ZhaoZMM <146093207+ZhaoZMM@users.noreply.github.com>
2024-05-15 12:22:15 -0400

    Update Rectangle.java

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,2 +1,38 @@
-package PACKAGE_NAME;public class Rectangle {
+public class Rectangle implements doSomething{
+   private int length;
+   private int width;
+
+
+   public Rectangle(int length, int width) {
+       this.length = length;
+       this.width = width;
+   }
+
+
+   public int getLength() {
+       return length;
+   }
+
+
+   public int getWidth() {
+       return width;
+   }
+
+
+   public double area() {
+       return (length * width);
+   }
+
+
+   public double perimeter() {
+       return (2 * length + 2 * width);
+   }
+
+
+   public int weirdNum(){
+       return length*width+length+width;
+   }
+   public void saySomething(){
+       System.out.println("im a rectangle, or a square. who knows...");
+   }
 }
